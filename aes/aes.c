9ad4cc1501af697312e53359df6460132ed3785a
Yibai Zhang <xm1994@gmail.com>
2015-11-16 23:01:03 +0800

    finished aes

--- aes/aes.c
+++ aes/aes.c
@@ -1,11 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "aes.h"
 
+const uint8_t SBOX[16][16] = 
+{
+    {0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76 },
+    {0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0 },
+    {0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15 },
+    {0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75 },
+    {0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84 },
+    {0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf },
+    {0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8 }, 
+    {0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2 },
+    {0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73 },
+    {0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb },
+    {0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79 },
+    {0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08 },
+    {0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a },
+    {0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e },
+    {0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf },
+    {0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16 } 
+};
 
-char gmult(char a, char b) {
+const uint8_t REVBOX[16][16] = 
+{
+    {0x52, 0x09, 0x6a, 0xd5, 0x30, 0x36, 0xa5, 0x38, 0xbf, 0x40, 0xa3, 0x9e, 0x81, 0xf3, 0xd7, 0xfb },
+    {0x7c, 0xe3, 0x39, 0x82, 0x9b, 0x2f, 0xff, 0x87, 0x34, 0x8e, 0x43, 0x44, 0xc4, 0xde, 0xe9, 0xcb },
+    {0x54, 0x7b, 0x94, 0x32, 0xa6, 0xc2, 0x23, 0x3d, 0xee, 0x4c, 0x95, 0x0b, 0x42, 0xfa, 0xc3, 0x4e },
+    {0x08, 0x2e, 0xa1, 0x66, 0x28, 0xd9, 0x24, 0xb2, 0x76, 0x5b, 0xa2, 0x49, 0x6d, 0x8b, 0xd1, 0x25 },
+    {0x72, 0xf8, 0xf6, 0x64, 0x86, 0x68, 0x98, 0x16, 0xd4, 0xa4, 0x5c, 0xcc, 0x5d, 0x65, 0xb6, 0x92 },
+    {0x6c, 0x70, 0x48, 0x50, 0xfd, 0xed, 0xb9, 0xda, 0x5e, 0x15, 0x46, 0x57, 0xa7, 0x8d, 0x9d, 0x84 },
+    {0x90, 0xd8, 0xab, 0x00, 0x8c, 0xbc, 0xd3, 0x0a, 0xf7, 0xe4, 0x58, 0x05, 0xb8, 0xb3, 0x45, 0x06 },
+    {0xd0, 0x2c, 0x1e, 0x8f, 0xca, 0x3f, 0x0f, 0x02, 0xc1, 0xaf, 0xbd, 0x03, 0x01, 0x13, 0x8a, 0x6b },
+    {0x3a, 0x91, 0x11, 0x41, 0x4f, 0x67, 0xdc, 0xea, 0x97, 0xf2, 0xcf, 0xce, 0xf0, 0xb4, 0xe6, 0x73 },
+    {0x96, 0xac, 0x74, 0x22, 0xe7, 0xad, 0x35, 0x85, 0xe2, 0xf9, 0x37, 0xe8, 0x1c, 0x75, 0xdf, 0x6e },
+    {0x47, 0xf1, 0x1a, 0x71, 0x1d, 0x29, 0xc5, 0x89, 0x6f, 0xb7, 0x62, 0x0e, 0xaa, 0x18, 0xbe, 0x1b },
+    {0xfc, 0x56, 0x3e, 0x4b, 0xc6, 0xd2, 0x79, 0x20, 0x9a, 0xdb, 0xc0, 0xfe, 0x78, 0xcd, 0x5a, 0xf4 },
+    {0x1f, 0xdd, 0xa8, 0x33, 0x88, 0x07, 0xc7, 0x31, 0xb1, 0x12, 0x10, 0x59, 0x27, 0x80, 0xec, 0x5f },
+    {0x60, 0x51, 0x7f, 0xa9, 0x19, 0xb5, 0x4a, 0x0d, 0x2d, 0xe5, 0x7a, 0x9f, 0x93, 0xc9, 0x9c, 0xef },
+    {0xa0, 0xe0, 0x3b, 0x4d, 0xae, 0x2a, 0xf5, 0xb0, 0xc8, 0xeb, 0xbb, 0x3c, 0x83, 0x53, 0x99, 0x61 },
+    {0x17, 0x2b, 0x04, 0x7e, 0xba, 0x77, 0xd6, 0x26, 0xe1, 0x69, 0x14, 0x63, 0x55, 0x21, 0x0c, 0x7d } 
+};
 
-    char p = 0, i = 0, hbs = 0;
+int Nb=4;// bytes per subgroup
+int Nk,Nr;// key bytes & round
+
+
+uint8_t gmult(uint8_t a, uint8_t b) {
+
+    uint8_t p = 0, i = 0, hbs = 0;
 
     for (i = 0; i < 8; i++) {
         if (b & 1) {
@@ -18,10 +62,10 @@ char gmult(char a, char b) {
         b >>= 1;
     }
 
-    return (char)p;
+    return (uint8_t)p;
 }
 
-void GF28_add(char a[], char b[], char d[]) {
+void GF28_add(uint8_t a[], uint8_t b[], uint8_t d[]) {
 
 	d[0] = a[0]^b[0];
 	d[1] = a[1]^b[1];
@@ -29,7 +73,7 @@ void GF28_add(char a[], char b[], char d[]) {
 	d[3] = a[3]^b[3];
 }
 
-int GF28_Mul(char * a, char* b, char* d)
+int GF28_Mul(uint8_t * a, uint8_t* b, uint8_t* d)
 {
     d[0] = gmult(a[0],b[0])^gmult(a[3],b[1])^gmult(a[2],b[2])^gmult(a[1],b[3]);
     d[1] = gmult(a[1],b[0])^gmult(a[0],b[1])^gmult(a[3],b[2])^gmult(a[2],b[3]);
@@ -38,9 +82,9 @@ int GF28_Mul(char * a, char* b, char* d)
     return 0;
 }
 
-char R[] = {0x02, 0x00, 0x00, 0x00};
+uint8_t R[] = {0x02, 0x00, 0x00, 0x00};
  
-char * Rcon(char i) {
+uint8_t * Rcon(uint8_t i) {
 	
 	if (i == 1) {
 		R[0] = 0x01; // x^(1-1) = x^0 = 1
@@ -56,36 +100,36 @@ char * Rcon(char i) {
 	return R;
 }
 
-int SubBytes(char *state)
+int SubBytes(uint8_t *state)
 {
     int i = 0, j = 0, t;
     for(; i<4; ++i)
     {
-        for(; j<Nb; ++j)
+        for(j=0; j<Nb; ++j)
         {
             t = state[i*Nb + j];
-            state[i*Nb + j] = SBOX[t&0xf0>>4][t&0x0f];
+            state[i*Nb + j] = SBOX[(t&0xf0)>>4][t&0x0f];
         }
     }
     return 0;
 }
 
-int RevSubBytes(char *state)
+int RevSubBytes(uint8_t *state)
 {
     int i = 0, j = 0, t;
     for(; i<4; ++i)
     {
-        for(; j<Nb; ++j)
+        for(j=0; j<Nb; ++j)
         {
             t = state[i*Nb + j];
-            state[i*Nb + j] = REVBOX[t&0xf0>>4][t&0x0f];
+            state[i*Nb + j] = REVBOX[(t&0xf0)>>4][t&0x0f];
         }
     }
     return 0;
 }
 
 
-int ShiftRows(char *state)
+int ShiftRows(uint8_t *state)
 {
     int i = 1, j = 0, k, t;
     for(; i<4; ++i)
@@ -103,7 +147,7 @@ int ShiftRows(char *state)
     return 0;
 }
 
-int RevShiftRows(char *state)
+int RevShiftRows(uint8_t *state)
 {
     int i = 1, j = 0, k, t;
     for(; i<4; ++i)
@@ -111,7 +155,7 @@ int RevShiftRows(char *state)
         for(j = 0; j <i ; ++j)
         {
             t = state[i * Nb + Nb - 1];
-            for(k=1; k<Nb; ++k)
+            for(k=Nb-1; k>0; --k)
             {
                 state[i*Nb + k] = state[i*Nb + k - 1];
             }
@@ -121,11 +165,11 @@ int RevShiftRows(char *state)
     return 0;
 }
 
-int MixColumns(char *state)
+int MixColumns(uint8_t *state)
 {
     int i,j=0;
-    char a[] = {0x02, 0x01, 0x01, 0x03}; // a(x) = {02} + {01}x + {01}x2 + {03}x3
-    char col[4], res[4];
+    uint8_t a[] = {0x02, 0x01, 0x01, 0x03}; // a(x) = {02} + {01}x + {01}x2 + {03}x3
+    uint8_t col[4], res[4];
 
     for (;j<Nb;j++)
     {
@@ -143,10 +187,10 @@ int MixColumns(char *state)
     return 0;
 }
 
-void RevMixColumns(char *state) {
+int RevMixColumns(uint8_t *state) {
 
-    char a[] = {0x0e, 0x09, 0x0d, 0x0b}; // a(x) = {0e} + {09}x + {0d}x2 + {0b}x3
-    char col[4], res[4];
+    uint8_t a[] = {0x0e, 0x09, 0x0d, 0x0b}; // a(x) = {0e} + {09}x + {0d}x2 + {0b}x3
+    uint8_t col[4], res[4];
     int i, j=0;
     for (; j < Nb; ++j) {
         for (i = 0; i < 4; ++i) {
@@ -159,11 +203,12 @@ void RevMixColumns(char *state) {
             state[Nb*i+j] = res[i];
         }
     }
+    return 0;
 }
 
-int AddRoundKey(char *state, char *key, int rnd)
+int AddRoundKey(uint8_t *state, uint8_t *key, int rnd)
 {
-    char c;
+    uint8_t c;
     
     for (c = 0; c < Nb; c++) {
         state[Nb*0+c] = state[Nb*0+c]^key[4*Nb*rnd+Nb*c+0];
@@ -174,9 +219,9 @@ int AddRoundKey(char *state, char *key, int rnd)
     return 0;
 }
 
-void rot_word(char *w) {
+void rot_word(uint8_t *w) {
 
-    char tmp;
+    uint8_t tmp;
     int i;
 
     tmp = w[0];
@@ -188,7 +233,7 @@ void rot_word(char *w) {
     w[3] = tmp;
 }
 
-void sub_word(char *w) {
+void sub_word(uint8_t *w) {
 
     int i;
 
@@ -197,11 +242,11 @@ void sub_word(char *w) {
     }
 }
 
-void KeyExpansion(char *key, char *w) {
+void KeyExpansion(uint8_t *key, uint8_t *w) {
 
-    char tmp[4];
-    char i;
-    char len = Nb*(Nr+1);
+    uint8_t tmp[4];
+    uint8_t i;
+    uint8_t len = Nb*(Nr+1);
 
     for (i = 0; i < Nk; i++) {
         w[4*i+0] = key[4*i+0];
@@ -236,13 +281,15 @@ void KeyExpansion(char *key, char *w) {
 }
 
 
-int encrypt(char *in, char *out, *w)
+int self_encrypt(uint8_t *in, uint8_t *out, uint8_t *w)
 {
-    char state[4*Nb];
+    uint8_t state[4*Nb];
     int r, i=0, j=0;
 
-    for (; i < 4; ++i) {
-        for (; j < Nb; ++j) {
+    for (; i < 4; ++i)
+    {
+        for (j = 0; j < Nb; ++j)
+        {
             state[Nb*i+j] = in[i+4*j];
         }
     }
@@ -268,13 +315,15 @@ int encrypt(char *in, char *out, *w)
     return 0;
 }
 
-int decrypt(char *in, char *out， *w)
+int self_decrypt(uint8_t *in, uint8_t *out, uint8_t *w)
 {
-    char state[4*Nb];
+    uint8_t state[4*Nb];
     int r, i=0, j=0;
 
-    for (; i < 4; i++) {
-        for (; j < Nb; j++) {
+    for (; i < 4; i++) 
+    {
+        for (j=0 ; j < Nb; j++)
+        {
             state[Nb*i+j] = in[i+4*j];
         }
     }
@@ -297,18 +346,20 @@ int decrypt(char *in, char *out， *w)
             out[i+4*j] = state[Nb*i+j];
         }
     }
+    return 0;
 }
 
-int AESEncrypt(char *src, char *dst, int length, int *rength, char *key, int keylength)
+int AESEncrypt(uint8_t *src, uint8_t *dst, int length, int *rlength, uint8_t *key, int keylength)
 {
     int nop=length%16, i;
+    nop = (16 - nop)%16;
     int slength = length + nop;
 
-    char *source = malloc(slength), *w;
-    memcpy_s(source, slength, src, length);
+    uint8_t *source = malloc(slength), *w;
+    memcpy(source, src, length);
     for(i = length; i<slength; ++i)
     {
-        source[i]=(char)nop;
+        source[i]=(uint8_t)nop;
     }
     switch (keylength) {
         default:
@@ -318,10 +369,10 @@ int AESEncrypt(char *src, char *dst, int length, int *rength, char *key, int key
     }
     w = malloc(Nb*(Nr+1)*4);
     KeyExpansion(key, w);
-    char *p = source;
-    for(i=0; i<n; i+=16)
+    uint8_t *p = source;
+    for(i=0; i<slength; i+=16)
     {
-        encrypt(source+i, dst+i, w);
+        self_encrypt(source+i, dst+i, w);
     }
     *rlength = slength;
     free(source);
@@ -329,12 +380,12 @@ int AESEncrypt(char *src, char *dst, int length, int *rength, char *key, int key
     return 0;
 }
 
-int AESDecrypt(char *src, char *dst, int length, int *rength, char *key, int keylength)
+int AESDecrypt(uint8_t *src, uint8_t *dst, int length, int *rlength, uint8_t *key, int keylength)
 {
     int nop=length%16, i;
     if(nop)
         return -1;
-
+    uint8_t *w;
     switch (keylength) {
         default:
         case 16: Nk = 4; Nr = 10; break;
@@ -343,12 +394,12 @@ int AESDecrypt(char *src, char *dst, int length, int *rength, char *key, int key
     }
     w = malloc(Nb*(Nr+1)*4);
     KeyExpansion(key, w);
-    char *p = src;
-    for(i=0; i<n; i+=16)
+    uint8_t *p = src;
+    for(i=0; i<length; i+=16)
     {
-        decrypt(source+i, dst+i, w);
+        self_decrypt(src+i, dst+i, w);
     }
-    int nop = dst[length];
+    nop = dst[length];
     for(i=length - nop - 1; i<length; ++i)
     {
         if(dst[i]!=nop)
--- aes/main.c
+++ aes/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+#include <string.h>
 
 #include "aes.h"
 #include "sha256.h"
 
 int main(int argc, char *argv[])
 {
-	int i, ch, opterr =0;
+	int i, ch;
 	int encrypt=0, decrypt=0;
 	char *filename = NULL, *password = NULL;
 	uint8_t buf[1024], out[1024];
@@ -18,15 +18,20 @@ int main(int argc, char *argv[])
 
 	int in_count, out_count, real_out;
 
-	while(ch = getopt(argc, argv,"cx"))
-	{
-		switch(ch)
-		{
-			case 'c': encrypt = 1; break;
-			case 'x': decrypt = 1; break;
-			default: printf("%s: Must specify one of -c, -x\n", argv[0]); return 1;
-		}
-	}
+    for(i=1;i<argc;i++)
+    {
+        if(!strcmp(argv[i],"-c"))
+        {
+            encrypt = 1;
+            continue;
+        }
+        if(!strcmp(argv[i],"-x"))
+        {
+            decrypt = 1;
+            continue;
+        }
+    }
+
 	for(i=1;i<argc;i++)
 	{
 		if(argv[i][0]!='-')
@@ -35,7 +40,8 @@ int main(int argc, char *argv[])
 			break;
 		}
 	}
-	for(i<argc;i++)
+    i++;
+    for(;i<argc;i++)
 	{
 		if(argv[i][0]!='-')
 		{
@@ -45,17 +51,17 @@ int main(int argc, char *argv[])
 	}
 	if (!filename || ! password)
 	{
-		printf ("Usage: %s [args] filename password\n");
+		printf ("Usage: %s [args] filename password\n",argv[0]);
 		return 1;
 	}
-	if(encrypt^decrypt)
+	if(!encrypt^decrypt)
 	{
 		printf("%s: Must specify one of -c, -x\n", argv[0]);
 		return 1;
 	}
 	
-	strcpy_s(outname, 256, filename);
-	strcat_s(outname, 256, ".out");
+	strcpy(outname, filename);
+	strcat(outname, ".out");
 	sha256_init(&ctx);
     sha256_hash(&ctx, (uint8_t *)password, (uint32_t)strlen(password));
     sha256_done(&ctx, hash);
